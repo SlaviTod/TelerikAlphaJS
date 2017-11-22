7550a0d9b648c7bdba031fc32e1934ef2724c272
SlaviTod <slavka_todorova@abv.bg>
2017-11-22 19:18:34 +0200

    95 points

--- 01. Weird bits/task.js
+++ 01. Weird bits/task.js
@@ -9,18 +9,49 @@ function solve(args){
     "Jul", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
     
     if(day !=1){
-        result = day+"-"+months[month-1] + "-"+ year
-    } 
+        let newDay = day-1;
+        result = newDay +"-"+months[month-1] + "-"+ year;
+    }else if(month !=1)
+    {
+        let newMonth = month-1;
+        let newDay = lastDayOfMonth(newMonth);
+        result = newDay +"-"+months[newMonth-1] + "-"+ year;
+    }else {
+        let newMonth = 12;
+        let newDay = lastDayOfMonth(newMonth);
+        let newYear = year-1;
+        result = newDay +"-"+months[newMonth-1] + "-"+ newYear;
+    }
+
+
 
     console.log(result);
+
+    function lastDayOfMonth(month)
+    {
+        if(month ===1 || month ===3 || month ===5 || month ===7 
+        || month ===8 || month ===10 || month ===12 )
+            {
+                return 31;
+            }else if ( month === 4 || month ===6 || month ===9
+            || month ===11 )
+            {
+                return 30;
+            }else if(year%4 ===0)
+            {
+                return 29;
+            } else {
+                return 28;
+            }
+    }
 }
 
 solve([
-    "2017", "10", "11"
+    "2000", "3", "1" //високосна 
 ]);
 solve([
-    "2005", "4", "24"
+    "2005", "4", "1"
 ]);
 solve([
-    "1999", "12", "31"
+    "1999", "1", "1"
 ]);
